487abea34066080047d556f1d447dd0d09608da9
João Victor <joaocarvoli.eng@alu.ufc.br>
2021-07-25 22:13:06 -0300

    adjusting front

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -1,15 +1,7 @@
-// add bootstrap css 
+
 import 'bootstrap/dist/css/bootstrap.css'
-// own css files here
-import "../css/customcss.css";
+
+//import "../css/customcss.css";
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
